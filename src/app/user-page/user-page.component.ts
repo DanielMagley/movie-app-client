7dc0575ffae72dd86b486b4d330a6512b491e42d
Daniel Magley <danielmagley@Daniels-MacBook-Pro.local>
2018-11-07 09:55:36 -0500

    ll

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,39 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { PageService } from '../page.service';
+import { Component, OnInit } from "@angular/core";
+import { PageService } from "../page.service";
 
 @Component({
-  selector: 'app-user-page',
-  templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  selector: "app-user-page",
+  templateUrl: "./user-page.component.html",
+  styleUrls: ["./user-page.component.scss"]
 })
 export class UserPageComponent implements OnInit {
- returns;
-  constructor(
-    private service: PageService 
-  ) { }
+  returns;
+  constructor(private service: PageService) {}
 
   ngOnInit() {
     this.service.getList().subscribe(data => {
       this.returns = data;
-      console.log(this.returns)
-      }
-    })
-  
+      console.log(this.returns);
+    });
+  }
 
-
-  deleteItem(id){
+  deleteItem(id) {
     this.service.delete(id).subscribe(data => {
-      console.log(data)
-    })
+      console.log(data);
+    });
     window.location.href = "/user-page";
   }
 
   updateItem(id, image) {
     this.service.update(id, image).subscribe(data => {
-      console.log(data)
-    })
+      console.log(data);
+    });
   }
-
-} 
-
-
+}
