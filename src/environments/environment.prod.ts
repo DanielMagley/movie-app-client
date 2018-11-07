0d3c72cfb057473905d1ce89533a208b535e4a33
Daniel Magley <danielmagley@Daniels-MacBook-Pro.local>
2018-11-07 17:00:58 -0500

    no line

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export let APIURL = "";
 
 switch (window.location.hostname) {
   // this is the deployed angular application
-  case " https://streamline-app-rb.herokuapp.com/":
+  case " https://streamline-app-rb.herokuapp.com":
     // this is the full url of your deployed API
     APIURL = "https://asc-streamline-server.herokuapp.com";
     break;
